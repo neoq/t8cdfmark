b5cea7caaa305c8cf154ced6d0cc6c2d435eb949
Jakob Hördt <jakoblhoerdt@gmail.com>
2022-11-24 17:52:31 +0100

    fix extract

--- multibenchmark/extract.py
+++ multibenchmark/extract.py
@@ -8,12 +8,12 @@ import config
 
 print("nodes,tasks_per_node,storage_mode,cmode,comm_mode,num_element_wise_variables,repetition,actual_information_bytes,seconds,throughput_B/s")
 for c in config.configurations():
-	workdir = pathlib.Path(f"nodes{nodes}-tasks_per_node{tasks_per_node}/{c.cmode}-{c.storage_mode}-{c.comm_mode}-{c.num_element_wise_variables}-{c.bytes_hint}/{c.repetition}")
+	workdir = pathlib.Path(f"nodes{c.nodes}-tasks_per_node{c.tasks_per_node}/{c.cmode}-{c.storage_mode}-{c.comm_mode}-{c.num_element_wise_variables}-{c.bytes_hint}/{c.repetition}")
 	
 	if not (workdir/"success").is_file():
 		print(f"{workdir} failed", file=sys.stderr)
 		continue
 	
 	results = json.load(open(workdir/"results.json", "r"))
-	print(f"{nodes},{tasks_per_node},{storage_mode},{cmode},{comm_mode},{repetition},{results["actual_information_byte/s"]},{results["second/s"]},{results["throughput_B/s"]}")
+	print(f"{c.nodes},{c.tasks_per_node},{c.storage_mode},{c.cmode},{c.comm_mode},{c.num_element_wise_variables},{c.repetition},{results['actual_information_bytes']},{results['seconds']},{results['throughput_B/s']}")
 
