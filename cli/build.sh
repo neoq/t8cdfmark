057da46e1bea68e4b62fd080bcb1aa888d103db0
neop <jakoblhoerdt@gmail.com>
2023-03-02 09:24:54 +0100

    optimioptimize build script

--- cli/build.sh
+++ cli/build.sh
@@ -2,4 +2,4 @@
 # get source dir
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
-find $SCRIPT_DIR -name "*.cpp" | xargs -n 1 -P 40 ccache mpicxx -std=c++17 -I/scratch/users/hpctraining60/master/t8code_install/include -c && mpicxx *.o -L/scratch/users/hpctraining60/master/t8code_install/lib -lt8 -lp4est -lsc -o t8cdfmark
+find $SCRIPT_DIR -name "*.cpp" | xargs -n 1 -P 40 ccache mpicxx -Wall -std=c++17 -g -O3 -Ipath/to/t8code_install/include -c && mpicxx *.o -Lpath/to/t8code_install/lib -lt8 -lp4est -lsc -o t8cdfmark
